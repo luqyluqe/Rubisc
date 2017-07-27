a98287e1b1614480745258a149fd9c367493342b
luqyluqe <luqy.luqe@gmail.com>
2017-07-27 17:13:15 +0800

    refactor

--- lib/fileutil.rb
+++ lib/fileutil.rb
@@ -28,5 +28,29 @@ module Rubisc
 			file.close
 			return true
 		end
+
+		def file_substitute file_path,old_content,new_content
+			process_file file_path,true do |content|
+				pattern=content.match /#{old_content}/
+				if !pattern
+					puts "No matching found."
+				end
+				content=content.gsub /#{old_content}/,new_content
+			end
+		end
+
+		def substitute path,old_content,new_content
+			if path!="." and path!=".."
+				if File.directory?(path)
+					Dir.entries(path).each do |sub|
+						if sub!="." and sub!=".."
+							dir_substitute "#{path}/#{sub}"
+						end
+					end
+				else
+					file_substitute path,old_content,new_content
+				end
+			end
+		end
 	end
 end
